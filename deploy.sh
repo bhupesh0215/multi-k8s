5551e9f35dc6a043b6744da80a82100ebfe30d89
bhupesh0215 <64361008+bhupesh0215@users.noreply.github.com>
2023-11-01 19:15:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
 
+docker build -t bhupesh0215/multi-client -f ./client/Dockerfile ./client
+docker build -t bhupesh0215/multi-server -f ./server/Dockerfile ./server
+docker build -t bhupesh0215/multi-worker -f ./worker /Dockerfile ./worker
