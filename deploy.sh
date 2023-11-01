9b8334ad1af282f7a5add89ac818a3e737768d95
bhupesh0215 <64361008+bhupesh0215@users.noreply.github.com>
2023-11-01 19:53:42 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,14 @@
 docker build -t bhupesh0215/multi-client -f ./client/Dockerfile ./client
 docker build -t bhupesh0215/multi-server -f ./server/Dockerfile ./server
 docker build -t bhupesh0215/multi-worker -f ./worker /Dockerfile ./worker
+
+docker login -u ${{ secrets.DOCKER_HUB_USERNAME }} -p ${{ secrets.DOCKER_HUB_PASSWORD }}
+
+docker push bhupesh0215/multi-client
+docker push bhupesh0215/multi-server
+docker push bhupesh0215/multi-worker
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=bhupesh0215/multi-server
+
